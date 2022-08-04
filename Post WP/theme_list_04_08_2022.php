8d08cdee8b1ea74ef58cda520724de46980dc73c
roni7032 <padukaroni@yahoo.com>
2022-08-04 16:22:48 +0700

    bismillah 04082022

--- Post WP/theme_list_04_08_2022.php
+++ Post WP/theme_list_04_08_2022.php
@@ -1,7 +1,7 @@
 <?php $theme_array = array(
 array(
         "id" 		 => "reach-digital",
-        "url" 		 => "https://bimberonline.com/reach/?storefront=envato-elements",
+        "url" 		 => "https://bimberonline.com/reach/",
         "purchase" 	 => "/downloads/reach-digital-agency-creative-theme/",
         "responsive" => "yes",
         "type"       => "wordpress",
@@ -9,7 +9,7 @@ array(
     ),
 array(
         "id" 		 => "rumma-furniture",
-        "url" 		 => "https://elementorkits.nathatype.com/rumma/?storefront=envato-elements",
+        "url" 		 => "https://elementorkits.nathatype.com/rumma/",
         "purchase" 	 => "/downloads/rumma-furniture-interior-theme/",
         "responsive" => "yes",
         "type"       => "wordpress",
@@ -17,23 +17,15 @@ array(
     ),
 array(
         "id" 		 => "runda-creative",
-        "url" 		 => "https://kit.juliha.com/runda/home/?storefront=envato-elements",
+        "url" 		 => "https://kit.juliha.com/runda/home/",
         "purchase" 	 => "/downloads/runda-creative-agency-theme/",
         "responsive" => "yes",
         "type"       => "wordpress",
         "thumb"      => ""
     ),
 array(
-        "id" 		 => "singgah-architecture",
-        "url" 		 => "http://templatekit.esensifiksi.com/singgah/?storefront=envato-elements",
-        "purchase" 	 => "/downloads/singgah-architecture-theme/",
-        "responsive" => "yes",
-        "type"       => "wordpress",
-        "thumb"      => ""
-    ),
-array(
         "id" 		 => "skape-creative",
-        "url" 		 => "https://wordpressriverthemes.com/skape/?storefront=envato-elements",
+        "url" 		 => "https://wordpressriverthemes.com/skape/",
         "purchase" 	 => "/downloads/skape-creative-digital-agency-business-theme/",
         "responsive" => "yes",
         "type"       => "wordpress",
@@ -41,7 +33,7 @@ array(
     ),
 array(
         "id" 		 => "sociabuff-social",
-        "url" 		 => "https://demo.moxcreative.com/sociabuff/template-kit/homepage/?storefront=envato-elements",
+        "url" 		 => "https://demo.moxcreative.com/sociabuff/template-kit/homepage/",
         "purchase" 	 => "/downloads/students-online-ecourse-website/",
         "responsive" => "yes",
         "type"       => "wordpress",
@@ -49,8 +41,72 @@ array(
     ),
 array(
         "id" 		 => "socialy-social",
-        "url" 		 => "https://templatekit.tokomoo.com/socialkit/?storefront=envato-elements",
+        "url" 		 => "https://templatekit.tokomoo.com/socialkit/",
         "purchase" 	 => "/downloads/socialy-social-media-marketing-agency-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "spora-digital",
+        "url" 		 => "https://show.moxcreative.com/spora/template-kit/homepage/",
+        "purchase" 	 => "/downloads/spora-digital-agency-creative-themes/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "sputnik-socialmedia",
+        "url" 		 => "https://bimberonline.com/sputnik/",
+        "purchase" 	 => "/downloads/sputnik-social-media-marketing-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "startx-creative",
+        "url" 		 => "https://templatekit.brothergrounds.com/startx/",
+        "purchase" 	 => "/downloads/startx-creative-agency-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "strack-multipurpose",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout34/",
+        "purchase" 	 => "/downloads/strack-multipurpose-business-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "syntra-seodigital",
+        "url" 		 => "https://templatekit.jegtheme.com/syntra/",
+        "purchase" 	 => "/downloads/syntra-seo-digital-marketing-agency-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "teko-creative",
+        "url" 		 => "https://meem-codex.site/teko/template-kit/home/",
+        "purchase" 	 => "/downloads/teko-creative-agency-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "telce-creative",
+        "url" 		 => "https://surielementor.com/telcokit/template-kit/home/",
+        "purchase" 	 => "/downloads/telce-creative-agency-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "umi-webdesign",
+        "url" 		 => "https://template.coomb.co/umi/",
+        "purchase" 	 => "/downloads/umi-web-design-agency-theme/",
         "responsive" => "yes",
         "type"       => "wordpress",
         "thumb"      => ""
