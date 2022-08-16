56bb5bb295350310329d4debe6b17ed86b7e5987
roni7032 <padukaroni@yahoo.com>
2022-08-16 16:36:42 +0700

    bismillah

---
+++ Post WP/theme_list_15_08_2022.php
@@ -0,0 +1,138 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "fishco-fishing",
+        "url" 		 => "https://templatekit.kulokale.com/fishco/template-kit/home/",
+        "purchase" 	 => "/downloads/fishco-fishing-hunting-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fit-factoryfitness",
+        "url" 		 => "https://a.catand.us/fitfactory/",
+        "purchase" 	 => "/downloads/fit-factory-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fit-pointgym",
+        "url" 		 => "https://intothedesign.com/kits/kit-3/template-kit/home/",
+        "purchase" 	 => "/downloads/fit-point-gym-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fitgym-fitnessgym",
+        "url" 		 => "https://kinforce.net/fitgym/",
+        "purchase" 	 => "/downloads/fitgym-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fitnez-fitness",
+        "url" 		 => "https://demo.moxcreative.com/fitnez/template-kit/homepage/",
+        "purchase" 	 => "/downloads/fitnez-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fitx-fitnessgym",
+        "url" 		 => "https://yrkit.rometheme.pro/fitx/",
+        "purchase" 	 => "/downloads/fitx-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "flaire-danceschool",
+        "url" 		 => "https://templatekit.tokomoo.com/dancekit/",
+        "purchase" 	 => "/downloads/flaire-dance-school-studio-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "ggwepe-esports",
+        "url" 		 => "https://templatekit.jegtheme.com/ggwepe/",
+        "purchase" 	 => "/downloads/ggwepe-esports-gaming-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "greenside-golfclub",
+        "url" 		 => "https://demo.moxcreative.com/greenside/template-kit/home/",
+        "purchase" 	 => "/downloads/greenside-golf-club-academy-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gunomic-shooting",
+        "url" 		 => "https://templatekit.jegtheme.com/gunomic/",
+        "purchase" 	 => "/downloads/gunomic-shooting-range-gun-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gymax-gymfitness",
+        "url" 		 => "https://wordpress-landing.arrowtheme.com/gymax/",
+        "purchase" 	 => "/downloads/gymax-gym-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gymmy-fitness",
+        "url" 		 => "https://templatekit.pinisiart.com/fitnesskit/",
+        "purchase" 	 => "/downloads/gymmy-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gympact-fitness",
+        "url" 		 => "https://templatekit.jegtheme.com/gympact/",
+        "purchase" 	 => "/downloads/gympact-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gymsite-gym",
+        "url" 		 => "https://el-kits.bolvo.com/gymsite/template-kit/home-page/",
+        "purchase" 	 => "/downloads/gymsite-gym-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "horrses-equestrian",
+        "url" 		 => "https://templatekit.jegtheme.com/horrses/",
+        "purchase" 	 => "/downloads/horrses-equestrian-horse-riding-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "intera-footballteam",
+        "url" 		 => "https://templatekit.tokomoo.com/footballkit/",
+        "purchase" 	 => "/downloads/intera-football-team-sports-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "jaran-horseriding",
+        "url" 		 => "https://kit.nirmanavisual.com/jaran/template-kit/home/",
+        "purchase" 	 => "/downloads/jaran-horse-riding-school-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
