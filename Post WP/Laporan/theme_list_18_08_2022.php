779f6cea83581ddbcad6def64dada30fa444ef70
roni7032 <padukaroni@yahoo.com>
2022-08-19 16:19:52 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_18_08_2022.php
@@ -0,0 +1,114 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "sportstar-fitness",
+        "url" 		 => "https://template-kit.rootlayers.com/sportstar/template-kit/home/",
+        "purchase" 	 => "/downloads/sportstar-fitness-gym-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "tenez-tennisschool",
+        "url" 		 => "https://preview.moxcreative.com/tenez/template-kit/homepage/",
+        "purchase" 	 => "/downloads/tenez-tennis-school-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "tigerfist-martialarts",
+        "url" 		 => "https://preview.moxcreative.com/tigerfist/template-kit/homepage/",
+        "purchase" 	 => "/downloads/tigerfist-martial-arts-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "topchess-chessclub",
+        "url" 		 => "https://templatekit.jegtheme.com/topchess/",
+        "purchase" 	 => "/downloads/topchess-chess-club-courses-training-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "topspin-tennisschool",
+        "url" 		 => "https://templatekit.jegtheme.com/topspin/",
+        "purchase" 	 => "/downloads/topspin-tennis-school-sports-club-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "toronto-gym",
+        "url" 		 => "https://rockboystudio.com/template-demo/toronto/",
+        "purchase" 	 => "/downloads/toronto-gym-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "treina-personaltrainer",
+        "url" 		 => "https://templatekit.jegtheme.com/treina/",
+        "purchase" 	 => "/downloads/treina-personal-trainer-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "trena-sportfitness",
+        "url" 		 => "https://kit.wirastudio.co/trena/template-kit/home-1/",
+        "purchase" 	 => "/downloads/trena-sport-fitness-trainer-services-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "westwood-golfclub",
+        "url" 		 => "https://templatekit.jegtheme.com/westwood/",
+        "purchase" 	 => "/downloads/westwood-golf-club-course-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "workoution-sports",
+        "url" 		 => "https://tykit.rometheme.pro/workoution/",
+        "purchase" 	 => "/downloads/workoution-sports-and-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "xtreme-fitness",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout36/",
+        "purchase" 	 => "/downloads/xtreme-fitness-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "yogalaxy-yoga",
+        "url" 		 => "https://a.catand.us/yogalaxy/",
+        "purchase" 	 => "/downloads/yogalaxy-yoga-pilates-fitness-studio-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "yoganesia-yoga",
+        "url" 		 => "https://kreativ.space/yoganesia/template-kit/home/",
+        "purchase" 	 => "",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "yogasan-yogastudio",
+        "url" 		 => "https://kinforce.net/yogasan/",
+        "purchase" 	 => "/downloads/yogasan-yoga-studio-meditation-theme/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
