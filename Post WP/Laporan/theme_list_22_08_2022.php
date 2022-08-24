06447afd847b0d5a06b3a49b8572f9e0fc5a7dbc
roni7032 <padukaroni@yahoo.com>
2022-08-24 16:33:06 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_22_08_2022.php
@@ -0,0 +1,194 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "automaton-artificial",
+        "url" 		 => "https://show.moxcreative.com/automaton/template-kit/homepage/",
+        "purchase" 	 => "/downloads/automaton-artificial/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "autome-smarthome",
+        "url" 		 => "https://templatekit.tokomoo.com/smarthomekit/",
+        "purchase" 	 => "/downloads/autome-smarthome/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "axies-nftportfolio",
+        "url" 		 => "https://axieskit.demothemesflat.com/template-kit/home-01/",
+        "purchase" 	 => "/downloads/axies-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bidzen-nftsales",
+        "url" 		 => "https://demothemesflat.com/bidzenkit/template-kit/home-01/",
+        "purchase" 	 => "/downloads/bidzen-nftsales/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bigbytes-bigdata",
+        "url" 		 => "https://templatekit.jegtheme.com/bigbytes/",
+        "purchase" 	 => "/downloads/bigbytes-bigdata/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "booyah-creativeartist",
+        "url" 		 => "https://hrkit.rometheme.pro/booyah/",
+        "purchase" 	 => "/downloads/booyah-creativeartist/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "brandcoin-cryptocurrency",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout21/",
+        "purchase" 	 => "/downloads/brandcoin-cryptocurrency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bustic-techsoftware",
+        "url" 		 => "https://tykit.rometheme.pro/bustic/",
+        "purchase" 	 => "/downloads/bustic-techsoftware/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "carddio-cardpayment",
+        "url" 		 => "https://kit.juliha.com/carddio/home/",
+        "purchase" 	 => "/downloads/carddio-cardpayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cartera-personalnftportfolio",
+        "url" 		 => "https://tkpro-demo2.envalab.com/05/",
+        "purchase" 	 => "/downloads/cartera-personalnftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "clevehouse-smarthome",
+        "url" 		 => "https://templatekit.jegtheme.com/clevehouse/",
+        "purchase" 	 => "/downloads/clevehouse-smarthome/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "codex-cybersecurity",
+        "url" 		 => "https://rockboystudio.com/codex/template-kit/home/",
+        "purchase" 	 => "/downloads/codex-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "coinary-blockchain",
+        "url" 		 => "https://templatekit.pinisiart.com/criptokit/",
+        "purchase" 	 => "/downloads/coinary-blockchain/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "coinyx-cryptocurrency",
+        "url" 		 => "https://dkkit.rometheme.pro/coinyx/",
+        "purchase" 	 => "/downloads/coinyx-cryptocurrency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "craftmaya-itsolutions",
+        "url" 		 => "https://demo.moxcreative.com/craftmaya/template-kit/homepage-2/",
+        "purchase" 	 => "/downloads/craftmaya-itsolutions/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "credigi-digitalbank",
+        "url" 		 => "https://templatekit.jegtheme.com/credigi/",
+        "purchase" 	 => "/downloads/credigi-digitalbank/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cripto-cryptocurrency",
+        "url" 		 => "https://kinforce.net/cripto/",
+        "purchase" 	 => "/downloads/cripto-cryptocurrency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cryptiva-cybersecurity",
+        "url" 		 => "https://templatekit.jegtheme.com/cryptiva/",
+        "purchase" 	 => "/downloads/cryptiva-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cryptodax-cryptocurrency",
+        "url" 		 => "https://templatekit.puriwp.com/cryptodax/template-kit/home/",
+        "purchase" 	 => "/downloads/cryptodax-cryptocurrency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cryptopia-nftcrypto",
+        "url" 		 => "https://ibeydesign.com/cryptopia/",
+        "purchase" 	 => "/downloads/cryptopia-nftcrypto/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cyberjet-websecurity",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout22/",
+        "purchase" 	 => "/downloads/cyberjet-websecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cybershield-cyber",
+        "url" 		 => "https://templatekit.puriwp.com/cybershield/template-kit/home/",
+        "purchase" 	 => "/downloads/cybershield-cyber/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cybertech-cyber",
+        "url" 		 => "https://elkit.rootlayers.com/cybertech/",
+        "purchase" 	 => "/downloads/cybertech-cyber/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "datera-onlinedating",
+        "url" 		 => "https://templatekit.jegtheme.com/datera/",
+        "purchase" 	 => "/downloads/datera-onlinedating/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
