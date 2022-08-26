9c7ff9a2f150279f7d18766a5d8acb1a60aec5ab
roni7032 <padukaroni@yahoo.com>
2022-08-26 07:43:38 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_25_08_2022.php
@@ -0,0 +1,218 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "debite-cardpayment",
+        "url" 		 => "https://dkkit.rometheme.pro/debite/",
+        "purchase" 	 => "/downloads/debite-cardpayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "deepay-cardpayment",
+        "url" 		 => "https://hrkit.rometheme.pro/deepay/",
+        "purchase" 	 => "/downloads/deepay-cardpayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "devvata-nftportfolio",
+        "url" 		 => "https://templatekit.jegtheme.com/devvata/",
+        "purchase" 	 => "/downloads/devvata-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dezuvo-nftportfolio",
+        "url" 		 => "https://dkkit.rometheme.pro/dezuvo/",
+        "purchase" 	 => "/downloads/dezuvo-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "digit-robotics",
+        "url" 		 => "https://dkkit.rometheme.pro/digit/",
+        "purchase" 	 => "/downloads/digit-robotics/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "digitron-softwaresaas",
+        "url" 		 => "https://digitron.radiantthemes.com/template-kit/home-version-one/",
+        "purchase" 	 => "/downloads/digitron-softwaresaas/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "digity-digitalagency",
+        "url" 		 => "https://kits.artstudioworks.net/digity/",
+        "purchase" 	 => "/downloads/digity-digitalagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "disrupt-techstartup",
+        "url" 		 => "https://templatekit.jegtheme.com/disrupt/",
+        "purchase" 	 => "/downloads/disrupt-techstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "doqu-darkdigital",
+        "url" 		 => "https://templatekit.jegtheme.com/doqu/",
+        "purchase" 	 => "/downloads/doqu-darkdigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "droid-roboticstechnology",
+        "url" 		 => "https://demo.moxcreative.com/droid/template-kit/homepage/",
+        "purchase" 	 => "/downloads/droid-roboticstechnology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dronix-saasstartup",
+        "url" 		 => "https://dronix.radiantthemes.com/",
+        "purchase" 	 => "/downloads/dronix-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "easychat-onlineapp",
+        "url" 		 => "https://elementorkits.nathatype.com/easychat/",
+        "purchase" 	 => "/downloads/easychat-onlineapp/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "envotek-itsolution",
+        "url" 		 => "https://kit-demos.envalab.com/envotek/",
+        "purchase" 	 => "/downloads/envotek-itsolution/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "essential-photography",
+        "url" 		 => "https://hrkit.rometheme.pro/essential/",
+        "purchase" 	 => "/downloads/essential-photography/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "evernet-broadbandinternet",
+        "url" 		 => "https://templatekit.jegtheme.com/evernet/",
+        "purchase" 	 => "/downloads/evernet-broadbandinternet/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "everpay-onlinepayment",
+        "url" 		 => "https://templatekit.jegtheme.com/everpay/",
+        "purchase" 	 => "/downloads/everpay-onlinepayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "expanet-broadband",
+        "url" 		 => "https://demo.moxcreative.com/expanet/template-kit/homepage/",
+        "purchase" 	 => "/downloads/expanet-broadband/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fastnet-broadband",
+        "url" 		 => "https://tykit.rometheme.pro/fastnet/",
+        "purchase" 	 => "/downloads/fastnet-broadband/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fiscale-business",
+        "url" 		 => "https://templatekit.tokomoo.com/appkit/",
+        "purchase" 	 => "/downloads/fiscale-business/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "foruum-saassoftware",
+        "url" 		 => "https://templatekit.tokokoo.com/sasskit/",
+        "purchase" 	 => "/downloads/foruum-saassoftware/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "frinex-blockchain",
+        "url" 		 => "https://templatekit.tokomoo.com/cryptokit/",
+        "purchase" 	 => "/downloads/frinex-blockchain/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gatech-cybersecurity",
+        "url" 		 => "https://templatekit.tokomoo.com/cyberkit/",
+        "purchase" 	 => "/downloads/gatech-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "greentech-greentechnology",
+        "url" 		 => "https://show.moxcreative.com/greentech/template-kit/homepage/",
+        "purchase" 	 => "/downloads/greentech-greentechnology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "habitate-smarthome",
+        "url" 		 => "https://show.moxcreative.com/habitate/template-kit/home/",
+        "purchase" 	 => "/downloads/habitate-smarthome/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hape-gadgetshowcase",
+        "url" 		 => "https://gajean.com/hape/template-kit/homepage/",
+        "purchase" 	 => "/downloads/hape-gadgetshowcase/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hasto-cybertech",
+        "url" 		 => "https://demo.moxcreative.com/hasto/template-kit/homepage/",
+        "purchase" 	 => "/downloads/hasto-cybertech/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hecxa-creativedesign",
+        "url" 		 => "https://templatekit.jegtheme.com/hecxa/",
+        "purchase" 	 => "/downloads/hecxa-creativedesign/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
