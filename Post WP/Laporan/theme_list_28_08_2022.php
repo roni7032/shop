aa81b4f2e0bc6994c7e5a841b8a68161e5813235
roni7032 <padukaroni@yahoo.com>
2022-08-30 05:55:50 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_28_08_2022.php
@@ -0,0 +1,58 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "hekel-cybersecurity",
+        "url" 		 => "https://kreativ.space/hekel/template-kit/home/",
+        "purchase" 	 => "/downloads/hekel-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hostic-webhosting",
+        "url" 		 => "https://templatekit.jegtheme.com/hostic/",
+        "purchase" 	 => "/downloads/hostic-webhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hostlabs-cloudhosting",
+        "url" 		 => "https://tkpro-demos.envalab.com/02/template-kit/hostlabs-home/",
+        "purchase" 	 => "/downloads/hostlabs-cloudhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hostsavvy-webhosting",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout07/",
+        "purchase" 	 => "/downloads/hostsavvy-webhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hosty-hostingservices",
+        "url" 		 => "https://kits.pixel-show.com/hosty/",
+        "purchase" 	 => "/downloads/hosty-hostingservices/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hostyng-webhosting",
+        "url" 		 => "https://kinforce.net/hostyng/",
+        "purchase" 	 => "/downloads/hostyng-webhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "imagine-creative",
+        "url" 		 => "https://hrkit.rometheme.pro/imagine/",
+        "purchase" 	 => "/downloads/imagine-creative/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
