9622c28acbb22d76474aaccbc82d822ad53ceb3b
roni7032 <padukaroni@yahoo.com>
2022-09-08 07:03:55 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_07_09_2022.php
@@ -0,0 +1,506 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "integra-itsolution",
+        "url" 		 => "https://elkit.rootlayers.com/integra/",
+        "purchase" 	 => "/downloads/integra-itsolution/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "izeetak-itsolutions",
+        "url" 		 => "https://surielementor.com/izeetakkit/template-kit/home-01/",
+        "purchase" 	 => "/downloads/izeetak-itsolutions/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "izpay-mobile",
+        "url" 		 => "https://templatekit.jegtheme.com/izpay/",
+        "purchase" 	 => "/downloads/izpay-mobile/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "knox-technology",
+        "url" 		 => "https://yrkit.rometheme.pro/knox/",
+        "purchase" 	 => "/downloads/knox-technology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "koneksi-homewifi",
+        "url" 		 => "https://eightheme.com/koneksi/template-kit/home/",
+        "purchase" 	 => "/downloads/koneksi-homewifi/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "maxinet-internetprovider",
+        "url" 		 => "https://template-kit.ancorathemes.com/maxinet-internet-provider/template-kit/home-1-pro/",
+        "purchase" 	 => "/downloads/maxinet-internetprovider/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "mecell-internetconnection",
+        "url" 		 => "https://kit.wirastudio.co/mecell/template-kit/home-layout-1/",
+        "purchase" 	 => "/downloads/mecell-internetconnection/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "meddaz-saasmedical",
+        "url" 		 => "https://kreativ.space/meddaz/template-kit/home/",
+        "purchase" 	 => "/downloads/meddaz-saasmedical/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "medup-medical",
+        "url" 		 => "https://kreativ.space/medup/template-kit/home/",
+        "purchase" 	 => "/downloads/medup-medical/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "miber-techcompany",
+        "url" 		 => "https://elementor.nathatype.com/",
+        "purchase" 	 => "/downloads/miber-techcompany/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "mindtech-itsolutions",
+        "url" 		 => "https://kit.baliniz.com/mindtech/",
+        "purchase" 	 => "/downloads/mindtech-itsolutions/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "misternft-nftportfolio",
+        "url" 		 => "https://tkpro-demo2.envalab.com/07/template-kit/home/",
+        "purchase" 	 => "/downloads/misternft-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "monax-saasstartup",
+        "url" 		 => "https://meem-codex.site/monax/template-kit/home/",
+        "purchase" 	 => "/downloads/monax-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "monztera-nftportfolio",
+        "url" 		 => "https://monztera.1onestrong.com/",
+        "purchase" 	 => "/downloads/monztera-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "moxpay-onlinepayment",
+        "url" 		 => "https://demo.moxcreative.com/moxpay/template-kit/homepage/",
+        "purchase" 	 => "/downloads/moxpay-onlinepayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "namex-cybersecurity",
+        "url" 		 => "https://kit.juliha.com/namex/home/",
+        "purchase" 	 => "/downloads/namex-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "neftize-nftportfolio",
+        "url" 		 => "https://show.moxcreative.com/neftize/template-kit/homepage/",
+        "purchase" 	 => "/downloads/neftize-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "netric-proxyvpn",
+        "url" 		 => "https://templatekit.jegtheme.com/netric/",
+        "purchase" 	 => "/downloads/netric-proxyvpn/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "netzo-nftportfolio",
+        "url" 		 => "https://preview.moxcreative.com/netzo/template-kit/homepage/",
+        "purchase" 	 => "/downloads/netzo-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "payplus-mobile",
+        "url" 		 => "https://templatekit.tokomoo.com/mobilekit/",
+        "purchase" 	 => "/downloads/payplus-mobile/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "nftx-nftportfolio",
+        "url" 		 => "https://kit.pixel-show.com/ntf/",
+        "purchase" 	 => "/downloads/nftx-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "nifty-nftproject",
+        "url" 		 => "https://templatekits.themewarrior.com/nifty/template-kit/home/",
+        "purchase" 	 => "/downloads/nifty-nftproject/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "nusapp-saasstartup",
+        "url" 		 => "https://template-kit.rootlayers.com/nusapp/",
+        "purchase" 	 => "/downloads/nusapp-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "paylite-onlinepayment",
+        "url" 		 => "https://elkit.rootlayers.com/paykit/template-kit/home/",
+        "purchase" 	 => "/downloads/paylite-onlinepayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "paynet-digitalewallet",
+        "url" 		 => "https://paynet.1onestrong.com/",
+        "purchase" 	 => "/downloads/paynet-digitalewallet/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "protel-broadband",
+        "url" 		 => "https://protel.radiantthemes.com/template-kit/home-version-one/",
+        "purchase" 	 => "/downloads/protel-broadband/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "reeze-nftportfolio",
+        "url" 		 => "https://promo-theme.com/reeze-kit/",
+        "purchase" 	 => "/downloads/reeze-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "revius-energy",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout30/",
+        "purchase" 	 => "/downloads/revius-energy/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "ryse-seodigitalmarketing",
+        "url" 		 => "https://rysekit.radiantthemes.com/template-kit/home-version-one/",
+        "purchase" 	 => "/downloads/ryse-seodigitalmarketing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "saasio-saasstartup",
+        "url" 		 => "https://kits.pixel-show.com/saasio/",
+        "purchase" 	 => "/downloads/saasio-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "saastek-saastechnology",
+        "url" 		 => "https://saastek.radiantthemes.com/home-page-one/",
+        "purchase" 	 => "/downloads/saastek-saastechnology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "saasy-saasapp",
+        "url" 		 => "https://jthemes.net/themes/wp/elementor-kits/saasy/",
+        "purchase" 	 => "/downloads/saasy-saasapp/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "saep-ecommerceapp",
+        "url" 		 => "https://gajean.com/saep/template-kit/home-1/",
+        "purchase" 	 => "/downloads/saep-ecommerceapp/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "securs-cybersecurity",
+        "url" 		 => "https://dkkit.rometheme.pro/securs/",
+        "purchase" 	 => "/downloads/securs-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "serity-cctvsecurity",
+        "url" 		 => "https://geniuscript.com/serity-kit/index.php/template-kit/home-page/",
+        "purchase" 	 => "/downloads/serity-cctvsecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "shevia-softwaredeveloper",
+        "url" 		 => "https://tkpro-demos.envalab.com/22/template-kit/home/",
+        "purchase" 	 => "/downloads/shevia-softwaredeveloper/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "softek-softwareit",
+        "url" 		 => "https://softekkit.radiantthemes.com/",
+        "purchase" 	 => "/downloads/softek-softwareit/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "softup-saasstartup",
+        "url" 		 => "https://kits.pixel-show.com/softup/",
+        "purchase" 	 => "/downloads/softup-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "sparxup-saasstartup",
+        "url" 		 => "https://demo.moxcreative.com/sparxup/template-kit/homepage/",
+        "purchase" 	 => "/downloads/sparxup-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "startio-saasdigital",
+        "url" 		 => "https://kits.pixel-show.com/startio/",
+        "purchase" 	 => "/downloads/startio-saasdigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "startly-startupssaas",
+        "url" 		 => "https://demo.web3canvas.com/template-kits/startly/",
+        "purchase" 	 => "/downloads/startly-startupssaas/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "stellar-hostingcloud",
+        "url" 		 => "https://templatekits.themewarrior.com/stellar/template-kit/home/",
+        "purchase" 	 => "/downloads/stellar-hostingcloud/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "supernft-nftportfolio",
+        "url" 		 => "https://elkit.rootlayers.com/supernft/",
+        "purchase" 	 => "/downloads/supernft-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "techoes-techsoftware",
+        "url" 		 => "https://kit.juliha.com/techoes/home/",
+        "purchase" 	 => "/downloads/techoes-techsoftware/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "techplus-techbusiness",
+        "url" 		 => "https://kraftmaya.com/techplus/",
+        "purchase" 	 => "/downloads/techplus-techbusiness/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "techtial-itsolutions",
+        "url" 		 => "https://templatekit.jegtheme.com/techtial/",
+        "purchase" 	 => "/downloads/techtial-itsolutions/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "techup-technologyit",
+        "url" 		 => "https://kits.pixel-show.com/techup/",
+        "purchase" 	 => "/downloads/techup-technologyit/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "teqno-techagency",
+        "url" 		 => "https://teqno.whmcsdes.com/",
+        "purchase" 	 => "/downloads/teqno-techagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "tflat-nftportfolio",
+        "url" 		 => "https://tykit.rometheme.pro/tflat/",
+        "purchase" 	 => "/downloads/tflat-nftportfolio/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "uzino-startupapp",
+        "url" 		 => "https://surielementor.com/uzinokit/template-kit/home/",
+        "purchase" 	 => "/downloads/uzino-startupapp/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "vanguard-cybersecurity",
+        "url" 		 => "https://demo.moxcreative.com/vanguard/template-kit/homepage/",
+        "purchase" 	 => "/downloads/vanguard-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "veapp-mobileapp",
+        "url" 		 => "https://kits.pixel-show.com/veapp/",
+        "purchase" 	 => "/downloads/veapp-mobileapp/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "vibranet-broadbandinternet",
+        "url" 		 => "https://kit.baliniz.com/vibranet/",
+        "purchase" 	 => "/downloads/vibranet-broadbandinternet/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "vitech-technology",
+        "url" 		 => "https://templatekit.jegtheme.com/vitech/",
+        "purchase" 	 => "/downloads/vitech-technology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "wartel-homewifi",
+        "url" 		 => "https://gajean.com/wartel/template-kit/home-1/",
+        "purchase" 	 => "/downloads/wartel-homewifi/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "webhoster-webhosting",
+        "url" 		 => "https://demo.moxcreative.com/webhoster/template-kit/homepage/",
+        "purchase" 	 => "/downloads/webhoster-webhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "xarris-saasstartup",
+        "url" 		 => "https://templatekit.jegtheme.com/xarris/",
+        "purchase" 	 => "/downloads/xarris-saasstartup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "xsapp-saascompany",
+        "url" 		 => "https://xsaap.templatekit.co/",
+        "purchase" 	 => "/downloads/xsapp-saascompany/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "xshapp-applanding",
+        "url" 		 => "https://kit-demos.envalab.com/xshapp/",
+        "purchase" 	 => "/downloads/xshapp-applanding/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "yakopay-onlinepayment",
+        "url" 		 => "https://elementor.din-studio.com/yakopay/",
+        "purchase" 	 => "/downloads/yakopay-onlinepayment/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zamzam-applanding",
+        "url" 		 => "https://kit-demos.envalab.com/zamzam-app-landing/",
+        "purchase" 	 => "/downloads/zamzam-applanding/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zelo-startupbusiness",
+        "url" 		 => "https://demo.moxcreative.com/zelo/template-kit/homepage/",
+        "purchase" 	 => "/downloads/zelo-startupbusiness/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zortex-broadband",
+        "url" 		 => "https://bimberonline.com/zortex/",
+        "purchase" 	 => "/downloads/zortex-broadband/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
