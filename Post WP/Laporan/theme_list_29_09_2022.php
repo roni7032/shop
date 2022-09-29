7a0f485bf0aa3579b4fb914409dff225c781df40
roni7032 <padukaroni@yahoo.com>
2022-09-30 04:38:08 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_29_09_2022.php
@@ -0,0 +1,514 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "exodio-recording",
+        "url" 		 => "https://templatekit.tokomoo.com/recordkit/",
+        "purchase" 	 => "/downloads/exodio-recording/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "afro-mentorshipcoaching",
+        "url" 		 => "https://hrkit.rometheme.pro/afro/",
+        "purchase" 	 => "/downloads/afro-mentorshipcoaching/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "antibugs-pestcontrol",
+        "url" 		 => "https://point.moxcreative.com/antibugs/template-kit/homepage/",
+        "purchase" 	 => "/downloads/antibugs-pestcontrol/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "aquanus-yachtclub",
+        "url" 		 => "https://point.moxcreative.com/aquanus/template-kit/homepage/",
+        "purchase" 	 => "/downloads/aquanus-yachtclub/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "ashton-webhosting",
+        "url" 		 => "https://kitpro.site/ashton/",
+        "purchase" 	 => "/downloads/ashton-webhosting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "astroleeu-astrology",
+        "url" 		 => "https://tkpro-demo2.envalab.com/23/",
+        "purchase" 	 => "/downloads/astroleeu-astrology/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "automobi-autoparts",
+        "url" 		 => "https://templatekit.jegtheme.com/automobi/",
+        "purchase" 	 => "/downloads/automobi-autoparts/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "baby-doctorpediatric",
+        "url" 		 => "https://bimberonline.com/baby-doctor/",
+        "purchase" 	 => "/downloads/baby-doctorpediatric/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "baristar-baristacourses",
+        "url" 		 => "https://templatekit.jegtheme.com/baristar/",
+        "purchase" 	 => "/downloads/baristar-baristacourses/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bazzer-influencermarketing",
+        "url" 		 => "https://templatekit.jegtheme.com/bazzer/",
+        "purchase" 	 => "/downloads/bazzer-influencermarketing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "beeparty-kidsparty",
+        "url" 		 => "https://point.moxcreative.com/beeparty/template-kit/homepage/",
+        "purchase" 	 => "/downloads/beeparty-kidsparty/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "berona-printingdesign",
+        "url" 		 => "https://point.moxcreative.com/berona/template-kit/homepage/",
+        "purchase" 	 => "/downloads/berona-printingdesign/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bitzcoin-cryptoexchange",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout39/",
+        "purchase" 	 => "/downloads/bitzcoin-cryptoexchange/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "",
+        "url" 		 => "",
+        "purchase" 	 => "",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "branchous-plantgarden",
+        "url" 		 => "https://templatekit.jegtheme.com/branchous/",
+        "purchase" 	 => "/downloads/branchous-plantgarden/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "buildery-constructionbuilding",
+        "url" 		 => "https://templatekit.jegtheme.com/buildery/",
+        "purchase" 	 => "/downloads/buildery-constructionbuilding/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "careo-elderlysenior",
+        "url" 		 => "https://careo.radiantthemes.com/",
+        "purchase" 	 => "/downloads/careo-elderlysenior/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cargox-logistictransportation",
+        "url" 		 => "https://promo-theme.com/cargox/",
+        "purchase" 	 => "/downloads/cargox-logistictransportation/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "catlux-paintingwallpapering",
+        "url" 		 => "https://kitpro.site/catlux/",
+        "purchase" 	 => "/downloads/catlux-paintingwallpapering/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "clamby-fashionecommerce",
+        "url" 		 => "https://kreativ.space/clamby/template-kit/home/",
+        "purchase" 	 => "/downloads/clamby-fashionecommerce/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cleanse-cleaningservices",
+        "url" 		 => "https://tykit.rometheme.pro/cleanse/",
+        "purchase" 	 => "/downloads/cleanse-cleaningservices/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "clearview-eyeglasseseyewear",
+        "url" 		 => "https://templatekits.themewarrior.com/clearview/template-kit/home/",
+        "purchase" 	 => "",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "codiux-onlinecourse",
+        "url" 		 => "https://tykit.rometheme.pro/codiux/",
+        "purchase" 	 => "",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "coinary-blockchaincryptocurrency",
+        "url" 		 => "https://templatekit.pinisiart.com/criptokit/",
+        "purchase" 	 => "/downloads/coinary-blockchaincryptocurrency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "collab-onlinecourses",
+        "url" 		 => "https://collab.templatekit.co/",
+        "purchase" 	 => "/downloads/collab-onlinecourses/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "consultic-businessagency",
+        "url" 		 => "https://consultic.radiantthemes.com/",
+        "purchase" 	 => "/downloads/consultic-businessagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "creativo-creativeagency",
+        "url" 		 => "https://sparklethings.unspokenthings.co/creativo/",
+        "purchase" 	 => "/downloads/creativo-creativeagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dogworks-dogtraining",
+        "url" 		 => "https://bimberonline.com/dogworks/",
+        "purchase" 	 => "/downloads/dogworks-dogtraining/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cyber-nftmarketplace",
+        "url" 		 => "https://puzzlerbox.com/templatekits-free/nft-marketplace-portfolio/",
+        "purchase" 	 => "/downloads/cyber-nftmarketplace/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cybershield-cybersecurity",
+        "url" 		 => "https://templatekit.puriwp.com/cybershield/template-kit/home/",
+        "purchase" 	 => "/downloads/cybershield-cybersecurity/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dadigo-digitalagency",
+        "url" 		 => "https://elementor.din-studio.com/dadigo/",
+        "purchase" 	 => "/downloads/dadigo-digitalagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "darkle-creativedigital",
+        "url" 		 => "https://dkkit.rometheme.pro/darkle/",
+        "purchase" 	 => "/downloads/darkle-creativedigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "depuppy-pettraining",
+        "url" 		 => "https://dkkit.rometheme.pro/depuppy/",
+        "purchase" 	 => "/downloads/depuppy-pettraining/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "digitalskill-ecourseonline",
+        "url" 		 => "https://kreativ.space/digitalskill/template-kit/home/",
+        "purchase" 	 => "/downloads/digitalskill-ecourseonline/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "docbook-onlinebookstore",
+        "url" 		 => "https://templatekit.jegtheme.com/docbook/",
+        "purchase" 	 => "/downloads/docbook-onlinebookstore/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dotokai-karate",
+        "url" 		 => "https://templatekit.jegtheme.com/dotokai/",
+        "purchase" 	 => "/downloads/dotokai-karate/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dullet-shootingrange",
+        "url" 		 => "https://themesflat.com/dulletkit/template-kit/home-01/",
+        "purchase" 	 => "/downloads/dullet-shootingrange/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "emoji-eventagency",
+        "url" 		 => "https://emoji.templatekit.co/",
+        "purchase" 	 => "/downloads/emoji-eventagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "estatic-realestate",
+        "url" 		 => "https://demo.eightheme.com/estatic/template-kit/home/",
+        "purchase" 	 => "/downloads/estatic-realestate/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gymniac-fitnessexercise",
+        "url" 		 => "https://templatekit.jegtheme.com/gymniac/",
+        "purchase" 	 => "/downloads/gymniac-fitnessexercise/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "ezectric-electricalsupply",
+        "url" 		 => "https://templatekit.jegtheme.com/ezectric/",
+        "purchase" 	 => "/downloads/ezectric-electricalsupply/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "fesshion-fashionclothing",
+        "url" 		 => "https://tkpro-demo2.envalab.com/25/",
+        "purchase" 	 => "/downloads/fesshion-fashionclothing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "finanzo-financeconsulting",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout42/",
+        "purchase" 	 => "/downloads/finanzo-financeconsulting/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "flashnet-broadbandtelecom",
+        "url" 		 => "https://tykit.rometheme.pro/flashnet/",
+        "purchase" 	 => "/downloads/flashnet-broadbandtelecom/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "flights-privatejets",
+        "url" 		 => "https://kfkit.rometheme.pro/flights/",
+        "purchase" 	 => "/downloads/flights-privatejets/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "florazo-woocommerceflorist",
+        "url" 		 => "https://fullkit.moxcreative.com/florazo/template-kit/homepage/",
+        "purchase" 	 => "/downloads/florazo-woocommerceflorist/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "flywing-droneaerial",
+        "url" 		 => "https://templatekits.themewarrior.com/flywing/template-kit/homepage/",
+        "purchase" 	 => "/downloads/flywing-droneaerial/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "framarket-digitalmarketing",
+        "url" 		 => "https://elementorkits.nathatype.com/framarket/",
+        "purchase" 	 => "/downloads/framarket-digitalmarketing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "geegs-contentcreator",
+        "url" 		 => "https://tykit.rometheme.pro/geegs/",
+        "purchase" 	 => "/downloads/geegs-contentcreator/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gimigimi-woocommmerce",
+        "url" 		 => "https://sparklethings.unspokenthings.co/gimigimi/",
+        "purchase" 	 => "/downloads/gimigimi-woocommmerce/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "gotow-towingservices",
+        "url" 		 => "https://point.moxcreative.com/gotow/template-kit/homepage/",
+        "purchase" 	 => "/downloads/gotow-towingservices/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hairsta-hairsalon",
+        "url" 		 => "https://templatekit.jegtheme.com/hairsta/",
+        "purchase" 	 => "/downloads/hairsta-hairsalon/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hashtag-socialmedia",
+        "url" 		 => "https://demo.eightheme.com/hashtag/template-kit/home/",
+        "purchase" 	 => "/downloads/hashtag-socialmedia/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hiro-humanresources",
+        "url" 		 => "https://kit.baliniz.com/hiro/",
+        "purchase" 	 => "/downloads/hiro-humanresources/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "hygienex-cleaningservices",
+        "url" 		 => "https://www.hygienex.hocud.com/",
+        "purchase" 	 => "/downloads/hygienex-cleaningservices/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "influenser-influencermarketing",
+        "url" 		 => "https://tkpro-demo2.envalab.com/30/template-kit/home/",
+        "purchase" 	 => "/downloads/influenser-influencermarketing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "insuranlife-insuranceagency",
+        "url" 		 => "https://insuranlife.1onestrong.com/",
+        "purchase" 	 => "/downloads/insuranlife-insuranceagency/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "intense-creativedigital",
+        "url" 		 => "https://demo.eightheme.com/intense/template-kit/home/",
+        "purchase" 	 => "/downloads/intense-creativedigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "juana-fashionstore",
+        "url" 		 => "https://touringxx.com/wptemplatekits/newjuana/template-kit/home/",
+        "purchase" 	 => "/downloads/juana-fashionstore/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "livin-interiordesign",
+        "url" 		 => "https://templatekit.hellokuro.com/livin/",
+        "purchase" 	 => "/downloads/livin-interiordesign/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "keymaster-locksmith",
+        "url" 		 => "https://point.moxcreative.com/keymaster/template-kit/homepage/",
+        "purchase" 	 => "/downloads/keymaster-locksmith/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "kitchenia-kitchenware",
+        "url" 		 => "https://templatekit.jegtheme.com/kitchenia/",
+        "purchase" 	 => "/downloads/kitchenia-kitchenware/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "klinskin-skincare",
+        "url" 		 => "https://tkpro-demo2.envalab.com/26/template-kit/home/",
+        "purchase" 	 => "/downloads/klinskin-skincare/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "kulit-skincare",
+        "url" 		 => "https://tkpro-demo2.envalab.com/22/",
+        "purchase" 	 => "/downloads/kulit-skincare/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
