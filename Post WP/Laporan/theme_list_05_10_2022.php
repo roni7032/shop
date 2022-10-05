c8a90834732a342d5b32f9d26f4fdfa760073fec
roni7032 <padukaroni@yahoo.com>
2022-10-05 07:15:17 +0700

    bismillah

---
+++ Post WP/Laporan/theme_list_05_10_2022.php
@@ -0,0 +1,210 @@
+<?php $theme_array = array(
+array(
+        "id" 		 => "solargy-solaralternative",
+        "url" 		 => "https://solargy.radiantthemes.com/",
+        "purchase" 	 => "/downloads/solargy-solaralternative/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "strack-bizel",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout38/",
+        "purchase" 	 => "/downloads/strack-bizel/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "synauw-onlinecourse",
+        "url" 		 => "https://elementor.deverust.com/synauw/template-kit/homepage/",
+        "purchase" 	 => "/downloads/synauw-onlinecourse/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "topchess-chessclub",
+        "url" 		 => "https://templatekit.jegtheme.com/topchess/",
+        "purchase" 	 => "/downloads/topchess-chessclub/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "traverse-traveltour",
+        "url" 		 => "https://demo.eightheme.com/traverse/template-kit/home/",
+        "purchase" 	 => "/downloads/traverse-traveltour/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "urayana-universitycollege",
+        "url" 		 => "https://kit.baliniz.com/urayana/",
+        "purchase" 	 => "/downloads/urayana-universitycollege/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "vironic-augmentedvirtual",
+        "url" 		 => "https://templatekit.jegtheme.com/vironic/",
+        "purchase" 	 => "/downloads/vironic-augmentedvirtual/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "voiz-musicmanagement",
+        "url" 		 => "https://templatekit.jegtheme.com/voiz/",
+        "purchase" 	 => "/downloads/voiz-musicmanagement/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "wastco-wastepickup",
+        "url" 		 => "https://templatekit.jegtheme.com/wastco/",
+        "purchase" 	 => "/downloads/wastco-wastepickup/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "wineful-winestore",
+        "url" 		 => "https://templatekit.jegtheme.com/wineful/",
+        "purchase" 	 => "/downloads/wineful-winestore/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "yogashala-yogameditation",
+        "url" 		 => "https://demo.bosathemes.com/yogashala/",
+        "purchase" 	 => "/downloads/yogashala-yogameditation/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zembolic-branddesign",
+        "url" 		 => "https://elementor.deverust.com/zembolic/template-kit/homepage/",
+        "purchase" 	 => "/downloads/zembolic-branddesign/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zenovis-creativedigital",
+        "url" 		 => "https://templatekit.jegtheme.com/zenovis/",
+        "purchase" 	 => "/downloads/zenovis-creativedigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zoorilla-safarizoo",
+        "url" 		 => "https://templatekit.tokomoo.com/zookit/",
+        "purchase" 	 => "/downloads/zoorilla-safarizoo/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "zooventour-safarizoo",
+        "url" 		 => "https://point.moxcreative.com/zooventour/template-kit/homepage/",
+        "purchase" 	 => "/downloads/zooventour-safarizoo-2/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "autowing-emergencytowing",
+        "url" 		 => "https://kitpro.site/autowing/",
+        "purchase" 	 => "/downloads/autowing-emergencytowing/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "battlebugs-pestcontrol",
+        "url" 		 => "https://mhkit.rometheme.pro/battlebugs/",
+        "purchase" 	 => "/downloads/battlebugs-pestcontrol/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "bbw-buildingbetter",
+        "url" 		 => "https://puzzlerbox.com/templatekits-free/design-studio/",
+        "purchase" 	 => "/downloads/bbw-buildingbetter/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "cognitive-psychologycounseling",
+        "url" 		 => "https://templatekit.jegtheme.com/cognitive/",
+        "purchase" 	 => "/downloads/cognitive-psychologycounseling/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dshirt-tshirtdesign",
+        "url" 		 => "https://dkkit.rometheme.pro/dshirt/",
+        "purchase" 	 => "/downloads/dshirt-tshirtdesign/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dakota-creativedigital",
+        "url" 		 => "https://tykit.rometheme.pro/dakota/",
+        "purchase" 	 => "/downloads/dakota-creativedigital/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dentistkit-dentalclinic",
+        "url" 		 => "https://elkit.rootlayers.com/dentistkit/template-kit/home/",
+        "purchase" 	 => "/downloads/dentistkit-dentalclinic/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dexen-printingcopy",
+        "url" 		 => "https://surielementor.com/dexenkit/template-kit/home-01/",
+        "purchase" 	 => "/downloads/dexen-printingcopy/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "diverge-airobotics",
+        "url" 		 => "https://elementorpress.com/templatekit-pro/layout45/",
+        "purchase" 	 => "",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "dorze-customdress",
+        "url" 		 => "https://tkpro-demo2.envalab.com/40/",
+        "purchase" 	 => "/downloads/dorze-customdress/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+array(
+        "id" 		 => "educator-universityschool",
+        "url" 		 => "https://demo.bosathemes.com/educator/",
+        "purchase" 	 => "/downloads/educator-universityschool/",
+        "responsive" => "yes",
+        "type"       => "wordpress",
+        "thumb"      => ""
+    ),
+);
